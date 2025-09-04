82edb141bdbe00b3bb3eaa4cd7680223179c38d1
christianzaboli <zabolichristian@gmail.com>
2025-09-04 15:56:32 +0200

    esercizio finito con bonus

--- script/script.js
+++ script/script.js
@@ -1,9 +1,38 @@
+// creo la variabile che seleziona il div della lampadina
 const lampadina = document.querySelector('.lampadinaSpenta');
 
-const lightSwitch = document.querySelector('button');
+// creo la variabile che seleziona il button singolo
+const lightSwitch = document.getElementById('switch');
 
+// creo la variabile che seleziona il button alternato
+const lightSwitch1 = document.getElementById('switch1');
+console.log(lightSwitch1);
+
+const lightSwitch1text = document.getElementById('switch1').innerHTML;
+console.log(lightSwitch1text);
+
+// eventlistener per far funzionare il button singolo
 lightSwitch.addEventListener('click', 
     function () {
     lampadina.classList.add('lampadinaAccesa')
     }
 )
+
+
+// ---------- bonus ----------
+
+// eventlistener per far funzionare il button bonus
+lightSwitch1.addEventListener('click', 
+    function () {
+        if (lampadina.classList.contains('lampadinaSpenta')) {
+            lampadina.classList.add('lampadinaAccesa')
+            lampadina.classList.remove('lampadinaSpenta')
+            lightSwitch1.innerHTML = 'Spegni'
+        } else {
+            lampadina.classList.add('lampadinaSpenta')
+            lampadina.classList.remove('lampadinaAccesa')
+            lightSwitch1.innerHTML = 'Accendi'
+        }
+    }
+)
+
